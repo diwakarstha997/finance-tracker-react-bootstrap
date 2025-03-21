ad002100419c06852131501e12207797a25e67b7
diwakarstha997 <165546734+diwakarstha997@users.noreply.github.com>
2025-03-21 16:19:04 +1100

    Add footer content and design

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,13 +1,14 @@
-import { Col, Container, Row, Stack } from "react-bootstrap";
+import { Col, Row, Stack } from "react-bootstrap";
+import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
 import Logo from "./logo";
 import TextContent from "./textContent";
 
 const Footer = () => {
     return(
         <>
-            <Row className="">
-                <Col className="">
-                    <Stack>
+            <Row className="my-4">
+                <Col>
+                    <Stack gap={3}>
                         <div>
                            <Logo fontSize="32px"/>
                         </div>
@@ -16,17 +17,30 @@ const Footer = () => {
                             fontSize="18px"
                             minimumWidth="400px"
                         />
-                        <div>socials</div>
+                        <Stack direction="horizontal" gap={3}>
+                            <div style={{padding: "10px", height: "50px", width: "50px"}} className="border border-secondary rounded-circle text-center">
+                                <FaFacebookF />
+                            </div>
+                            <div style={{padding: "10px", height: "50px", width: "50px"}} className="border border-secondary rounded-circle text-center">
+                                <FaTwitter />
+                            </div>
+                            <div style={{padding: "10px", height: "50px", width: "50px"}} className="border border-secondary rounded-circle text-center">
+                                <FaInstagram />
+                            </div>
+                            <div style={{padding: "10px", height: "50px", width: "50px"}} className="border border-secondary rounded-circle text-center">
+                                <FaLinkedin />
+                            </div>
+                        </Stack>
                     </Stack>
                 </Col>
                 <Col>
-                    <Stack>
+                    <Stack gap={3}>
                         <h4>Company</h4>
-                        <ul className="footer-list">
-                            <li>Home</li>
-                            <li>About Us</li>
-                            <li>Contact</li>
-                        </ul>
+                        <Stack gap={2}>
+                            <div>Home</div>
+                            <div>About Us</div>
+                            <div>Contact</div>
+                        </Stack>
                     </Stack>
                 </Col>
             </Row>
