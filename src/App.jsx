9efc3905a4ad3f33e33919a39c2f71acb6b4ba4f
diwakarstha997 <165546734+diwakarstha997@users.noreply.github.com>
2025-03-21 13:30:36 +1100

    Add Hero and Quote sectin layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Container } from 'react-bootstrap'
+import { Button, Col, Container, Row, Stack } from 'react-bootstrap'
 import './App.css'
 import Navbar from './components/navbar'
 import Footer from './components/footer'
@@ -9,6 +9,29 @@ function App() {
     <>
       <Container>
         <Navbar/>
+
+        {/* Hero section */}
+        <section>
+          <Row className='bg-success'>
+            <Col className='bg-info'>
+              <div>Hero beginning text</div>
+              <div>Hero main text</div>
+              <div>Hero welcome text</div>
+              <div>
+                Button
+              </div>
+            </Col>
+            <Col>
+              Image
+            </Col>
+          </Row>
+        </section>
+
+        {/* Quote Section */}
+        <section>
+          <h1 className='text-center'>Landing Page Main text</h1>
+        </section>
+
         <Footer/>
       </Container>
     </>
