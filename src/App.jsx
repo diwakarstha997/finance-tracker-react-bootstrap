2bfd1656880fd93802e0bf949fbefa61c3c0ab5d
diwakarstha997 <165546734+diwakarstha997@users.noreply.github.com>
2025-03-21 13:36:40 +1100

    Add feature section layout

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,39 @@ function App() {
           <h1 className='text-center'>Landing Page Main text</h1>
         </section>
 
+        {/* Feature Section */}
+        <section>
+           {/* Feature 1 */}
+           <Row className='bg-success'>
+            <Col>
+              Image
+            </Col>
+            <Col className='bg-info'>
+              <div>Feature main text</div>
+              <div>Feature Description text</div> 
+              <hr/>
+              <ul>
+                <li>list 1</li>
+                <li>list 2</li>
+              </ul>
+            </Col>
+          </Row>
+
+          {/* Feature 2 */}
+          <Row className='bg-success'>
+            <Col className='bg-info'>
+            <div>Feature main text</div>
+            <div>Feature Description text</div> 
+              <div>
+                Button
+              </div>
+            </Col>
+            <Col>
+              Image
+            </Col>
+          </Row>
+        </section>
+
         <Footer/>
       </Container>
     </>
